3c692599595919a9cccc4e192a09cac591d86687
Brian Li <brnli7@gmail.com>
2021-11-02 07:24:41 -0700

    Emit event when rewardsDistribution changes

--- contracts/RewardsDistributionRecipient.sol
+++ contracts/RewardsDistributionRecipient.sol
@@ -9,6 +9,11 @@ import "./Owned.sol";
 abstract contract RewardsDistributionRecipient is Owned {
   address public rewardsDistribution;
 
+  event RewardsDistributionChanged(
+    address indexed previousRewardsDistribution,
+    address indexed nextRewardsDistribution
+  );
+
   function notifyRewardAmount(uint256 reward) external virtual;
 
   modifier onlyRewardsDistribution() {
@@ -23,6 +28,11 @@ abstract contract RewardsDistributionRecipient is Owned {
     external
     onlyOwner
   {
+    address previousRewardsDistribution = rewardsDistribution;
     rewardsDistribution = _rewardsDistribution;
+    emit RewardsDistributionChanged(
+      previousRewardsDistribution,
+      rewardsDistribution
+    );
   }
 }
