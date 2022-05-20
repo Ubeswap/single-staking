5c806e82344b57fbbcfc0c1c5337e051c36bb776
Ethan Yu <captainethan138@gmail.com>
2022-05-20 05:43:15 -0400

    reformatted owned.sol

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -4,42 +4,41 @@ pragma solidity ^0.8.3;
 
 // https://docs.synthetix.io/contracts/source/contracts/owned
 contract Owned {
-    address public owner;
-    address public nominatedOwner;
+  address public owner;  
+  address public nominatedOwner;
 
-    constructor(address _owner) {
-        require(_owner != address(0), "Owner address cannot be 0");
-        owner = _owner;
-        emit OwnerChanged(address(0), _owner);
-    }
+constructor(address _owner) {
+    require(_owner != address(0), "Owner address cannot be 0");
+    owner = _owner;
+    emit OwnerChanged(address(0), _owner);
+}
 
-    function nominateNewOwner(address _owner) external onlyOwner {
-        nominatedOwner = _owner;
-        emit OwnerNominated(_owner);
-    }
+function nominateNewOwner(address _owner) external onlyOwner {
+    nominatedOwner = _owner;
+    emit OwnerNominated(_owner);
+}
 
-    function acceptOwnership() external {
-        require(
-            msg.sender == nominatedOwner,
-            "You must be nominated before you can accept ownership"
-        );
-        emit OwnerChanged(owner, nominatedOwner);
-        owner = nominatedOwner;
-        nominatedOwner = address(0);
-    }
+function acceptOwnership() external {
+  require(
+    msg.sender == nominatedOwner,
+    "You must be nominated before you can accept ownership"
+  );
+  emit OwnerChanged(owner, nominatedOwner);
+  owner = nominatedOwner;
+  nominatedOwner = address(0);
+}
 
-    modifier onlyOwner() {
-        _;
-    }
+modifier onlyOwner() {
+  _;
+}
 
-    function _onlyOwner() private view {
-        require(
-            msg.sender == owner,
-            "Only the contract owner may perform this action"
-            //_onlyOwner();
-        );
-    } //WANT THIS
+function _onlyOwner() private view {
+  require(
+    msg.sender == owner,
+    "Only the contract owner may perform this action"
+    );
+}
 
-    event OwnerNominated(address newOwner);
-    event OwnerChanged(address oldOwner, address newOwner);
+event OwnerNominated(address newOwner);
+event OwnerChanged(address oldOwner, address newOwner);
 }
