719eec171ae478f6aa81dff0ba893d06f63f6e69
Ethan Yu <captainethan138@gmail.com>
2022-05-20 08:07:55 -0400

    fixed Voter formatting

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -6,58 +6,55 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@ubeswap/governance/contracts/interfaces/IVotingDelegates.sol";
+
 import "./interfaces/IRomulusDelegate.sol";
 
 contract Voter is Ownable {
-    using SafeERC20 for IERC20;
+  using SafeERC20 for IERC20;
 
-    IVotingDelegates public immutable votingToken;
-    IRomulusDelegate public immutable romulusDelegate;
-    address public controllerAddress;
+  IVotingDelegates public immutable votingToken;
+  IRomulusDelegate public immutable romulusDelegate;
+  address public controllerAddress;
 
-    constructor(
-        IVotingDelegates _votingToken,
-        IRomulusDelegate _romulusDelegate
-    ) {
-        votingToken = _votingToken;
-        romulusDelegate = _romulusDelegate;
-        _votingToken.delegate(address(this));
-    }
+  constructor(
+    IVotingDelegates _votingToken,
+    IRomulusDelegate _romulusDelegate
+  ) {
+    votingToken = _votingToken;
+    romulusDelegate = _romulusDelegate;
+    _votingToken.delegate(address(this));
+  }
 
-    /// @notice
-    function addVotes(uint256 amount) external onlyOwner{
-        IERC20(address(votingToken)).safeTransferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
-    }
+  /// @notice
+  function addVotes(uint256 amount) external onlyOwner{
+    IERC20(address(votingToken)).safeTransferFrom(
+      msg.sender,
+      address(this),
+      amount
+    );
+  }
 
+  function removeVotes(uint256 amount) external onlyOwner{
+    IERC20(address(votingToken)).safeTransfer(msg.sender, amount);
+  }
 
-    function removeVotes(uint256 amount) external onlyOwner{
-        IERC20(address(votingToken)).safeTransfer(msg.sender, amount);
-    }
+  function castVote(uint256 proposalId, uint8 support) external onlyOwner {
+    romulusDelegate.castVote(proposalId, support);
+  }
 
-    function castVote(uint256 proposalId, uint8 support)
-        external
-        onlyOwner
-    {
-        romulusDelegate.castVote(proposalId, support);
-    }
-
-    function propose(
-        address[] memory targets,
-        uint256[] memory values,
-        string[] memory signatures,
-        bytes[] memory calldatas,
-        string memory description
-    ) external onlyOwner{
-        romulusDelegate.propose(
-            targets,
-            values,
-            signatures,
-            calldatas,
-            description
-        );
-    }
+  function propose(
+    address[] memory targets,
+    uint256[] memory values,
+    string[] memory signatures,
+    bytes[] memory calldatas,
+    string memory description
+    ) external onlyOwner {
+      romulusDelegate.propose(
+      targets,
+      values,
+      signatures,
+      calldatas,
+      description
+    );
+  }
 }
